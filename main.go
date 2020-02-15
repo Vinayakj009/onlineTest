fa5d520b1a83f29f4af36a805e4cb6aedac3bd9d
vinayak <vinayakj009@gmail.com>
2020-02-15 12:26:12 +0530

    Managed to write proper working queue

--- main.go
+++ main.go
@@ -1,71 +1,63 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// queueItem holds a single item in the queue.
-type queueItem struct {
-	next *queueItem
-	data string
+type JSONQueue interface {
+	insert(string) int
+	fetch() (bool, string)
+	setWaitTime(int)
 }
 
-// // JSONQueue interface for json queue.
-// type JSONQueue interface {
-// 	getQueueSize() int
-// 	insert(string) int
-// 	fetch() string
-// 	setWaitTime(int)
-// }
-
-// jsonQueue holds the data related to the queue.
 type jsonQueue struct {
-	queueHead        *queueItem
-	queueTail        *queueItem
-	numberOfElements int
-}
-
-func (queue *jsonQueue) getQueueSize() int {
-	return queue.numberOfElements
+	queue    []string
+	waitTime int
 }
 
 func (queue *jsonQueue) insert(data string) int {
-	temp := new(queueItem)
-	temp.data = data
-	temp.next = queue.queueHead
-	queue.queueHead = temp
-	if queue.queueTail == nil {
-		queue.queueTail = temp
-	}
-	queue.numberOfElements++
-	return queue.numberOfElements
+	queue.queue = append(queue.queue, data)
+	return len(queue.queue)
 }
 
 func (queue *jsonQueue) fetch() (bool, string) {
-	if queue.queueTail == nil {
+	if len(queue.queue) == 0 {
 		return false, ""
 	}
-	temp := queue.queueTail
-	queue.queueTail = temp.next
-	queue.numberOfElements--
-	if queue.queueTail == nil {
-		queue.queueHead = nil
-	}
+	temp := queue.queue[0]
+	queue.queue[0] = ""
+	queue.queue = queue.queue[1:]
+	return true, temp
 }
 
-func NewJsonQueue() *jsonQueue {
-	temp := jsonQueue{}
-	temp.queueHead = nil
-	temp.queueTail = nil
-	temp.numberOfElements = 0
-	return &temp
+func (queue *jsonQueue) setWaitTime(data int) {
+	queue.waitTime = data
 }
 
 func main() {
-	test := queueItem{}
-	test.data = "This is a test"
-	test.next = &queueItem{}
-	test.next.data = "This is also a test"
-	fmt.Println(test.data)
-	fmt.Println(test.next.data)
+	temp := new(jsonQueue)
+	temp.insert("data1")
+	temp.insert("data2")
+	clear, temp1 := temp.fetch()
+	if clear {
+		fmt.Print("This is the first data ")
+		fmt.Println(temp1)
+	} else {
+		fmt.Println("Could not fetch 1st data")
+		return
+	}
+	clear, temp1 = temp.fetch()
+	if clear {
+		fmt.Print("This is the 2nd data ")
+		fmt.Println(temp1)
+	} else {
+		fmt.Println("Could not fetch 2nd data")
+		return
+	}
+	clear, temp1 = temp.fetch()
+	if clear {
+		fmt.Print("This is the 3rd data ")
+		fmt.Println(temp1)
+	} else {
+		fmt.Println("Could not fetch 3rd data")
+		return
+	}
 }
